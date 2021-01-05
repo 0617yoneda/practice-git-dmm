246b54192fc7ec7926a4fe94d2590c991fbd4a6b
0617yoneda <0617yoneda@gmail.com>
2021-01-05 05:44:26 +0000

    [add]admin追加練習

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admins, skip: :all
+  devise_scope :admin do
+    get 'admins/sign_in' => 'admins/sessions#new', as: 'new_admin_session'
+    post 'admins/sign_in' => 'admins/sessions#create', as: 'admin_session'
+    delete 'admins/sign_out' => 'admins/sessions#destroy', as: 'destroy_admin_session'
+  end
+
+  devise_for :customers
+
+
+  namespace :admins do
+    root "homes#top"
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :products, only: [:index, :new, :show, :create, :edit, :update]
+    resources :categories, only: [:create, :index, :edit, :update]
+    resources :orders, only: [:index, :show, :update]
+    resources :order_products, only: [:update]
+  end
+
+
 end
